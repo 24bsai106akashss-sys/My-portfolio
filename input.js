2f4831e4ce2f75692714b160761a9493e6ffde4d
24bsai106akashss-sys <24bsai106akashss@skacas.ac.in>
2025-09-08 12:41:44 +0530

    Create input.js

---
+++ input.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const sendButton = document.querySelector(".btn");
+
+  sendButton.addEventListener("click", function () {
+    const name = "AKASH S";
+    const email = "24bsai106akashss@skacas.ac.in";
+    const phone = "8838983632";
+
+    alert(`✅ Thank you ${name}! I’ll contact you at ${email}`);
+  });
+});
